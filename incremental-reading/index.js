fb815378a2bd94eab00054b5ebd208d8cfb87a50
Feng Ya <fengya04@126.com>
2018-01-18 23:36:15 +0800

    mask

---
+++ incremental-reading/index.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Incremental Reading
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  Incremental Reading
+// @author       Feng Ya
+// @match        https://www.instapaper.com/read/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+
+  GM_addStyle('.mask {background-color: black !important;}');
+
+  $('.highlight').click(function() {
+    $(this).toggleClass('mask');
+  });
+})();
