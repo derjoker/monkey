c389ff202d8b7220b870115b58662065c310086f
Feng Ya <fengya04@126.com>
2018-02-24 07:46:45 +0800

    cloze

--- incremental-reading/index.user.js
+++ incremental-reading/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Incremental Reading
 // @namespace    http://tampermonkey.net/
-// @version      0.7.1
+// @version      0.8.0
 // @description  Read. Recite.
 // @description:en  Read. Recite.
 // @author       Feng Ya
@@ -14,16 +14,100 @@
 
   // Your code here...
 
-  GM_addStyle('.mask {background-color: black !important;}');
+  GM_addStyle(`
+  .mask {
+    color: rgba(255, 242, 51, 0.298) !important;
+  }
 
-  $('.highlight').addClass('mask');
+  .wrong {
+    color: red !important;
+  }
 
-  $('body').click(function(event) {
+  .correct {
+    color: green !important;
+  }
+
+  .cloze {
+    position: absolute;
+    z-index: 9;
+    background: transparent;
+    border: none;
+  }
+  `);
+
+  console.log('Incremental Reading for Language Learning.');
+
+  const highlight = {
+    add() {
+      document.querySelectorAll('span.highlight').forEach(highlight => {
+        const id = highlight.getAttribute('data-api-id');
+        highlight.insertAdjacentHTML(
+          'beforebegin',
+          `<input class="cloze" data-api-id="${id}" type="text" style="width: ${
+            highlight.offsetWidth
+          }px" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">`
+        );
+      });
+    },
+    toggle() {
+      document.querySelectorAll('span.highlight').forEach(highlight => {
+        highlight.classList.toggle('mask');
+      });
+    },
+  };
+
+  const cloze = {
+    add() {
+      document.querySelectorAll('.cloze').forEach(cloze => {
+        const id = cloze.getAttribute('data-api-id');
+        const answer = document.querySelector(
+          `span.highlight[data-api-id="${id}"]`
+        );
+        cloze.addEventListener('keypress', event => {
+          answer.classList.remove('wrong');
+          answer.classList.remove('correct');
+
+          // Enter or Tab
+          if (event.keyCode === 13 || event.keyCode === 9) {
+            if (cloze.value !== answer.textContent)
+              answer.classList.add('wrong');
+            else answer.classList.add('correct');
+          }
+        });
+      });
+    },
+    clear() {
+      document.querySelectorAll('.cloze').forEach(cloze => {
+        cloze.parentNode.removeChild(cloze);
+      });
+    },
+  };
+
+  // remove old clozes (used only in development)
+  cloze.clear();
+
+  highlight.add();
+  highlight.toggle();
+  cloze.add();
+
+  let clozeMode = true;
+
+  document.body.addEventListener('click', event => {
     if (event.target.nodeName !== 'BODY') return;
 
     // Instapaper: Remove Note
-    if ($('div.highlight_popover.reveal').length > 0) return;
+    if (document.querySelectorAll('div.highlight_popover.reveal').length > 0)
+      return;
 
-    $('.highlight').toggleClass('mask');
+    if (clozeMode) {
+      cloze.clear();
+    } else {
+      highlight.add();
+      cloze.add();
+    }
+
+    highlight.toggle();
+
+    clozeMode = !clozeMode;
   });
 })();
