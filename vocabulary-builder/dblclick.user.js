aee30fb1cf7cacc42aa0ddf5a48c41cb55f8b0e3
Ya Feng <fengya04@126.com>
2019-03-19 17:36:20 +0800

    apple

--- vocabulary-builder/dblclick.user.js
+++ vocabulary-builder/dblclick.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Double Click
 // @namespace    derjoker
-// @version      0.0.4
+// @version      0.0.5
 // @description  Double Click to Search (Lookup)
 // @author       Feng Ya
 // @match        https://www.duden.de/*
 // @match        https://de.wikipedia.org/*
 // @match        https://*.fandom.com/de/*
 // @match        https://www.mydealz.de/*
+// @match        https://www.apple.com/de/*
 // @match        https://derjoker.github.io/vocabulary-builder/*
 // @grant        none
 // ==/UserScript==
